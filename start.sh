4b6bf0c6aaf13d9c2281700f281ac3b479abf66f
Jirayu Photisan <gdunghi@gmail.com>
2018-01-05 23:44:40 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -d --name proxy --net=host --hostname=proxy -v /cache:/cache -v /nginx_conf:/etc/nginx/conf.d:ro -v /www:/var/www/:ro  nginx
+docker run -d --name proxy_host --net=host --hostname=proxy -v /cache:/cache -v /nginx_conf:/etc/nginx/conf.d:ro -v /www:/var/www/:ro  nginx
